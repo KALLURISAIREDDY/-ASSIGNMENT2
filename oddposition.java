ad5c3fd3bde2a09ee913937304364f760e3cb08d
Sai Kumar Reddy kalluru <79197143+KALLURISAIREDDY@users.noreply.github.com>
2024-03-13 18:14:47 +0530

    oddposition.java

--- oddposition.java
+++ oddposition.java
@@ -6,11 +6,9 @@ public class oddposition {
 
     public static void printOddPositionChars(String text) {
         for (int i = 0; i < text.length(); i++) {
-            // Skip white spaces
             if (text.charAt(i) == ' ') {
                 continue;
             }
-            // Check if the position is odd
             if (i % 2 != 0) {
                 System.out.print(text.charAt(i));
             }
